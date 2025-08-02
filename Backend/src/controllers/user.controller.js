a8007f675a6e100ede03f3d5956ba2769749ec4e
Rachit Verma <rachit28082004@gmail.com>
2025-08-03 05:11:34 +0530

    bugs

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -4,8 +4,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import getDataUri from "../utils/datauri.js";
-import cloudinary from "../utils/cloudinary.js";
+
 
 const registerUser = asyncHandler(async (req, res) => {
   // inputs from the user
