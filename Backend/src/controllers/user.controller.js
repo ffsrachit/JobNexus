f5cf7c4e82ff8850605e89296781512106f9be95
Rachit Verma <rachit28082004@gmail.com>
2025-08-04 07:10:16 +0530

    err

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -119,9 +119,7 @@ const updateProfile = asyncHandler(async (req, res) => {
   const file = req.file;
 
   const fileUri = getDataUri(file);
-  const cloudResponse = await cloudinary.uploader.upload(fileUri.content, {
-  resource_type: "raw"
-});
+  const cloudResponse = await cloudinary.uploader.upload(fileUri.content);
   const userId = req.id;
 
   const updateData = {};
